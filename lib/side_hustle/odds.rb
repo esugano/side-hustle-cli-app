7a23d758dd126f90c26871d672af0a03cf0f2186
Emily S <emily.sugano@gmail.com>
2018-04-17 16:43:37 +0000

    Create odds.rb file with some test code

---
+++ lib/side_hustle/odds.rb
@@ -0,0 +1,16 @@
+class SideHustle::Odds
+
+
+  def self.test
+  puts "testing testing 1 2 3"
+
+  team_1 = self.new
+  team_1.name = "New England"
+  team_1.odds = "+500"
+
+  team_2 = self.new
+  team_2.name = "Philadelphia"
+  team_2.odds = "+850"
+  end
+
+end
