b0fc78feaa9c46160d7cc936c57cfc26756f2777
Emily S <emily.sugano@gmail.com>
2018-04-17 16:58:24 +0000

    Update odds.rb with more test code

--- lib/side_hustle/odds.rb
+++ lib/side_hustle/odds.rb
@@ -4,13 +4,34 @@ class SideHustle::Odds
   def self.test
   puts "testing testing 1 2 3"
 
+  date = "04/17"
+
   team_1 = self.new
   team_1.name = "New England"
-  team_1.odds = "+500"
+  team_1.time_2_odds = "#{date} +500"
+  team_1.time_2_odds = "#{date} +500"
+  team_1.time_3_odds = "#{date} +500"
+  team_1.time_4_odds = "#{date} +500"
+  team_1.time_5_odds = "#{date} +500"
+  team_1.time_6_odds = "#{date} +500"
+  team_1.time_7_odds = "#{date} +500"
+  team_1.time_8_odds = "#{date} +500"
+  team_1.time_9_odds = "#{date} +500"
+  team_1.time_10_odds = "#{date} +500"
+
 
   team_2 = self.new
   team_2.name = "Philadelphia"
-  team_2.odds = "+850"
+  team_2.time_1_odds = "#{date} +500"
+  team_2.time_2_odds = "#{date} +500"
+  team_2.time_3_odds = "#{date} +500"
+  team_2.time_4_odds = "#{date} +500"
+  team_2.time_5_odds = "#{date} +500"
+  team_2.time_6_odds = "#{date} +500"
+  team_2.time_7_odds = "#{date} +500"
+  team_2.time_8_odds = "#{date} +500"
+  team_2.time_9_odds = "#{date} +500"
+  team_2.time_10_odds = "#{date} +500"
   end
 
 end
