73fb5a1b866f72d3f59cf0b718a160be58d9e153
Emily S <emily.sugano@gmail.com>
2018-04-17 17:09:42 +0000

    Add attr_accessors and instance variables to lib/side_hustle/odds.rb

--- lib/side_hustle/odds.rb
+++ lib/side_hustle/odds.rb
@@ -1,5 +1,5 @@
 class SideHustle::Odds
-
+  attr_accessor :name, :time_1_odds, :time_2_odds, :time_3_odds, :time_4_odds, :time_5_odds, :time_6_odds, :time_7_odds, :time_8_odds, :time_9_odds, :time_10_odds
 
   def self.test
   puts "testing testing 1 2 3"
@@ -8,7 +8,7 @@ class SideHustle::Odds
 
   team_1 = self.new
   team_1.name = "New England"
-  team_1.time_2_odds = "#{date} +500"
+  team_1.time_1_odds = "#{date} +500"
   team_1.time_2_odds = "#{date} +500"
   team_1.time_3_odds = "#{date} +500"
   team_1.time_4_odds = "#{date} +500"
@@ -34,4 +34,7 @@ class SideHustle::Odds
   team_2.time_10_odds = "#{date} +500"
   end
 
+  puts team_1
+  puts team_2
+  
 end
