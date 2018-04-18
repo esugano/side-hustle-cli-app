768678281a85fcb97b36697dfd59023274af852e
Emily S <emily.sugano@gmail.com>
2018-04-18 17:16:30 +0000

    Add require links to new odds.rb file

--- lib/side_hustle.rb
+++ lib/side_hustle.rb
@@ -1,3 +1,3 @@
 require_relative "side_hustle/version"
-require_relative './side_hustle/cli'
-require_relative './side_hustle/odds'
+require_relative 'side_hustle/cli'
+require_relative 'side_hustle/odds'
