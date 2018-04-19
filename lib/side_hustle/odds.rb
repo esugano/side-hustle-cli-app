242c01436faceb78eedde9add7d4e6c1d78e96db
Emily S <emily.sugano@gmail.com>
2018-04-19 16:30:15 +0000

    Begin coding scraper methods

--- lib/side_hustle/odds.rb
+++ lib/side_hustle/odds.rb
@@ -9,5 +9,8 @@ class SideHustle::Odds
     puts "+500"
   end
 
-
+  def self.scrape_test
+    doc = Nokogiri::HTML(open("https://js.sbrfeeds.com/index.php/future?iframeid=iframe-odds-widget-5849247360332532&timezone=16&theme=future&markettypeid=311&odds-type=US&version=2.0&books=93,19,169,1096,999996,180,139&trackers=1518124627581.xml&event-id=3399565"))
+    binding.pry
+  end
 end
