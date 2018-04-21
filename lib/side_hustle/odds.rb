2292452e2fbf523bbacf6821357fe345b21d49dd
Emily S <emily.sugano@gmail.com>
2018-04-21 19:38:33 +0000

    Update scraper method to grab list of teams

--- lib/side_hustle/odds.rb
+++ lib/side_hustle/odds.rb
@@ -11,13 +11,12 @@ class SideHustle::Odds
 
   def self.scrape_test
     @list_teams = []
-    doc = Nokogiri::HTML(open("https://js.sbrfeeds.com/index.php/future?iframeid=iframe-odds-widget-5849247360332532&timezone=16&theme=future&markettypeid=311&odds-type=US&version=2.0&books=93,19,169,1096,999996,180,139&trackers=1518124627581.xml&event-id=3399565"))
-    doc.css('.sides').each do |team|
-      @list_teams <<  team.css('.team fitWidth').text
-# output is one continous line:
-#     New EnglandPhiladelphiaPittsburghGreen BayMinnesotaL.A. RamsSan FranciscoNew OrleansAtlantaHoustonJacksonvilleOaklandSeat
-# tleDallasL.A. ChargersKansas CityCarolinaDenverBaltimoreTennesseeDetroitTampa BayN.Y. GiantsIndianapolisWashingtonClevelandBu
-# ffaloArizonaCincinnatiMiamiChicagoN.Y. Jets""
+    #initial HTML
+    messy_code = Nokogiri::HTML(open("https://js.sbrfeeds.com/index.php/future?iframeid=iframe-odds-widget-5849247360332532&timezone=16&theme=future&markettypeid=311&odds-type=US&version=2.0&books=93,19,169,1096,999996,180,139&trackers=1518124627581.xml&event-id=3399565"))
+    messy_code.css('.sides').each do |teams|
+      team.css('.team').each do |ind_teams|
+        @list_teams << ind_teams.text
+      end
     end
     binding.pry
   end
