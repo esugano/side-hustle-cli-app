ab9b4a6304dd58e0ed9b6a44816234cc4aa746f0
Emily S <emily.sugano@gmail.com>
2018-04-23 22:50:07 +0000

    Add more scraping test code

--- lib/side_hustle/odds.rb
+++ lib/side_hustle/odds.rb
@@ -1,5 +1,5 @@
 class SideHustle::Odds
-  attr_accessor :list_teams, :team_names, :team_odd_scrape
+  attr_accessor :list_teams, :team_names, :team_odd_scrape, :test
 
   def self.team_list
     puts = ["New England Patriots", "San Francisco 49ers", "LA Rams", "Seattle Seahawaks"]
@@ -8,6 +8,14 @@ class SideHustle::Odds
   def self.team_odds(team)
     puts "+500"
   end
+
+ # def self.scrape_iframe_url
+ #   test = Nokogiri::HTML(open("https://www.sportsbookreview.com/betting-odds/ncaa-basketball"))
+ #   test = test.css('.htmlContent')
+ #   test = test.at_xpath('//iframe')
+ #  #  test = test.xpath('//src')
+ #   binding.pry
+ # end
 
   def self.scrape_test
     @team_names = []
