0a4324b5f3b4bae0a531cff024b51c36a4eccaba
Emily S <emily.sugano@gmail.com>
2018-04-26 03:02:31 +0000

    Update #team_list

--- lib/side_hustle/odds.rb
+++ lib/side_hustle/odds.rb
@@ -1,20 +1,21 @@
 class SideHustle::Odds
-  attr_accessor :list_teams, :team_names, :team_odd_scrape, :test
+  attr_accessor :list_teams, :team_names, :team_odd_scrape, :all
 
-  def self.team_list
-    puts = ["New England Patriots", "San Francisco 49ers", "LA Rams", "Seattle Seahawaks"]
+  @@all =
+
+  def self.new(scrape_hash)
+    @all = scrape_hash
+    end
+
+  def team_list
+    @@all.each_with_index do |(key, value), index|
+      puts "#{team[key][:name]}"
+    end
   end
 
   def self.team_odds(team)
     puts "+500"
   end
 
- # def self.scrape_iframe_url
- #   test = Nokogiri::HTML(open("https://www.sportsbookreview.com/betting-odds/ncaa-basketball"))
- #   test = test.css('.htmlContent')
- #   test = test.at_xpath('//iframe')
- #  #  test = test.xpath('//src')
- #   binding.pry
- # end
 
 end
