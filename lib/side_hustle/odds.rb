7da36c55ba4a2ea10cfb2ed55091a4d354368081
Emily S <emily.sugano@gmail.com>
2018-04-26 03:22:01 +0000

    Update #initialize

--- lib/side_hustle/odds.rb
+++ lib/side_hustle/odds.rb
@@ -1,14 +1,12 @@
 class SideHustle::Odds
-  attr_accessor :list_teams, :team_names, :team_odd_scrape, :all
+  attr_accessor :list_teams, :team_names, :team_odd_scrape
 
-  @@all =
+  def initialize(scrape_hash)
+    @list_teams = scrape_hash
+  end
 
-  def self.new(scrape_hash)
-    @all = scrape_hash
-    end
-
-  def team_list
-    @@all.each_with_index do |(key, value), index|
+  def self.list_teams
+    @list_teams.each_with_index do |(key, value), index|
       puts "#{team[key][:name]}"
     end
   end
