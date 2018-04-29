22396b84ccf2d78169bb98a6a07320a368d550bb
Emily S <emily.sugano@gmail.com>
2018-04-29 03:26:59 +0000

    Simplified scraper code to grab name and odds of each time at the same time

--- lib/side_hustle/scraper.rb
+++ lib/side_hustle/scraper.rb
@@ -1,16 +1,29 @@
 class SideHustle::Scraper
-  attr_accessor :messy_code, :scrape_team_names, :scrape_team_odds, :list_teams
+  attr_accessor :name, :odds
 
   def get_page
     Nokogiri::HTML(open("https://js.sbrfeeds.com/index.php/future?iframeid=iframe-odds-widget-5849247360332532&timezone=16&theme=future&markettypeid=311&odds-type=US&version=2.0&books=93,19,169,1096,999996,180,139&trackers=1518124627581.xml&event-id=3399565"))
   end
 
   def scrape_page
-    self.get_page.css(".sides").each do |side|
-      side
-    # binding.pry
-  end
-  end
+    self.get_page.css(".sides").each do |team_odd_combo|
+      #scrapes the name of the team
+      team_odd_combo.css('.team').each do |each_team|
+        @name = each_team.text
+      end
+      #scrapes the odds for the specific team
+      team_odd_combo.css('.lines').each do |each_team|
+         each_team.css('#book-19').each do |each_team_odds|
+          @odds = each_team_odds.search('span').text
+         end #ends line 14
+      end #ends line 13
+      #creates a SideHustle::Odds class with the name and odd
+      SideHustle::Odds.new(@name, @odds)
+    end #ends line 9
+  end #ends #scrape_page
+ end
+
+#SideHustle::Scraper.new.scrape_page
 
   # def make_teams
   #   self.scrape_page.css('.team').each do |teams|
@@ -59,4 +72,3 @@ class SideHustle::Scraper
   #   puts @list_teams
 #   # end
 #
- end
