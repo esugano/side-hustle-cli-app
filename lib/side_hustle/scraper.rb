3acb2bbf0e5afa39c62e73394c6943f736ed5928
Emily S <emily.sugano@gmail.com>
2018-05-02 04:06:29 +0000

    Remove useless comments

--- lib/side_hustle/scraper.rb
+++ lib/side_hustle/scraper.rb
@@ -10,7 +10,7 @@ class SideHustle::Scraper
     @odds = []
     @list_teams = []
     self.get_page.css(".sides").each do |team_odd_combo|
-      
+
       #scrapes the name of the team
       team_odd_combo.css('.team').each do |each_team|
         @names << each_team.text
@@ -33,51 +33,3 @@ class SideHustle::Scraper
  end
 
 #SideHustle::Scraper.new.scrape_page
-
-  # def make_teams
-  #   self.scrape_page.css('.team').each do |teams|
-  #     SideHustle::Odds.new_from_scrape_page("#{teams.text}")
-  # #   end
-  # end
-
-  # def make_teams
-  #   scrape_teams.each do |t|
-  #     t.css('.team').each do |indiv_teams|
-  #       SideHustle::Odds.new_from_scrape_page(indiv_teams.text)
-  #       binding.pry
-  #   end
-  # end
-
-  # def scrape_page
-  #   @scrape_team_names = []
-  #   @scrape_team_odds = []
-  #   @list_teams = {}
-  #
-  #   #initial scrape
-  #   @messy_code = Nokogiri::HTML(open("https://js.sbrfeeds.com/index.php/future?iframeid=iframe-odds-widget-5849247360332532&timezone=16&theme=future&markettypeid=311&odds-type=US&version=2.0&books=93,19,169,1096,999996,180,139&trackers=1518124627581.xml&event-id=3399565"))
-  #
-  #   #grabs list of teams and saves them into the @scrape_team_names array
-  #   @messy_code.css('.sides').each do |teams|
-  #     teams.css('.team').each do |indiv_teams|
-  #       @scrape_team_names << indiv_teams.text
-  #     end
-  #   end
-  #
-  #  #grabs odds for each individual team and saves them into the @scrape_team_odds array
-  #   @messy_code.css('.sides').each do |teams|
-  #     teams.css('.lines').each do |each_team|
-  #       each_team.css('#book-19').each do |odds|
-  #         @scrape_team_odds << odds.search('span').text
-  #       end
-  #     end
-  #   end
-  #
-  # #merge @scrape_team_names and @scrape_team_odds into one hash, @list_teams
-  #   @scrape_team_names.each_with_index do |value, index|
-  #     @list_teams["#{value}"] = {:name => value, :odds => @scrape_team_odds[index]}
-  #   end
-  #
-  #   #prints out @list_teams
-  #   puts @list_teams
-#   # end
-#
